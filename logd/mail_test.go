340e33e6444d1fb55c2cb3c912ae781259a56b41
fexeak@163.com <fexeak@163.com>
2021-09-25 11:04:47 +0800

    .

--- logd/mail_test.go
+++ logd/mail_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestSmtp_SendMail(t *testing.T) {
 	s := &Smtp{
-		From:    "ubt2012@163.com",
-		Key:     "xxxxxxx",
+		From:    "fexeak999k@163.com",
+		Key:     "xxx", // 注意是授权码，不是账号密码
 		Host:    "smtp.163.com",
 		Port:    "465",
 		To:      []string{"fexeak@163.com"},
